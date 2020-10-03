cdf4be4bad573d39ebc79811e0fbd3f9320f86bf
justindho <justinho515@gmail.com>
2020-10-03 12:25:56 -0700

    Add historical data endpoint

--- app.js
+++ app.js
@@ -31,47 +31,49 @@ app.get('/details/<ticker>', (req, res) => {
 })
 
 
-app.get('/api/companydescription/:ticker', (req, res) => {
-  const ticker = req.params.ticker.toUpperCase();
+let requestOptions = {
+  'method': 'GET',
+  'headers': {
+    'Content-Type': 'application/json',
+  }
+}
 
-  let requestOptions = {
-    'url': `https://api.tiingo.com/tiingo/daily/${ticker}?token=${TIINGO_API_KEY}`,
-    'method': 'GET',
-    'headers': {
-      'Content-Type': 'application/json',
-    }
-  };
 
+let returnResponse = (res, requestOptions) => {
   request(requestOptions, (error, response, body) => {
     let json = JSON.parse(body);
     if (response.statusCode === 200) {
       res.json(json);
     } else {
-      res.json({'error': true});
+      res.json({'error': true, 'errormsg': error, 'statusCode': response.statusCode});
     }
   });
+}
+
+
+app.get('/api/companydescription/:ticker', (req, res) => {
+  const ticker = req.params.ticker.toUpperCase();
+  requestOptions['url'] = `https://api.tiingo.com/tiingo/daily/${ticker}?token=${TIINGO_API_KEY}`;
+  returnResponse(res, requestOptions);
 })
 
 
 app.get('/api/latestprice/:ticker', (req, res) => {
   const ticker = req.params.ticker.toUpperCase();
+  requestOptions['url'] = `https://api.tiingo.com/iex/?tickers=${ticker}&token=${TIINGO_API_KEY}`;
+  returnResponse(res, requestOptions);
+})
 
-  let requestOptions = {
-    'url': `https://api.tiingo.com/iex/?tickers=${ticker}&token=${TIINGO_API_KEY}`,
-    'method': 'GET',
-    'headers': {
-      'Content-Type': 'application/json',
-    }
-  }
 
-  request(requestOptions, (error, response, body) => {
-    let json = JSON.parse(body);
-    if (response.statusCode === 200) {
-      res.json(json);
-    } else {
-      res.json({'error': true});
-    }
-  });
+app.get('/api/historicaldata/:ticker', (req, res) => {
+  const ticker = req.params.ticker.toUpperCase();
+  const resampleFreq = 'daily';
+  let date = new Date();
+  const startTime = new Date(date.setFullYear(date.getFullYear() - 2)); // go 2 years back
+  const startDate = `${startTime.getFullYear()}-${startTime.getMonth() + 1}-${startTime.getDate()}`;
+
+  requestOptions['url'] = `https://api.tiingo.com/tiingo/daily/${ticker}/prices?startDate=${startDate}&resampleFreq=${resampleFreq}&token=${TIINGO_API_KEY}`;
+  returnResponse(res, requestOptions);
 })
 
 
