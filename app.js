53c47d8ae5e17035edd3c8de7e7499d6036ec04c
justindho <justinho515@gmail.com>
2020-10-03 11:39:01 -0700

    Add company description endpoint

--- app.js
+++ app.js
@@ -1,23 +1,55 @@
+require('dotenv').config();
+
 const express = require('express');
+const path = require('path');
+const request = require('request');
+
+const TIINGO_API_KEY = process.env.TIINGO_API_KEY;
+const NEWS_API_KEY = process.env.NEWS_API_KEY;
+
 const app = express();
 const port = 3000;
-const path = require('path');
 
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
 
+
 app.get('/watchlist', (req, res) => {
   res.send('Hello from watchlist');
 })
+
 
 app.get('/portfolio', (req, res) => {
   res.send('Hello from portfolio');
 })
 
+
 app.get('/details/<ticker>', (req, res) => {
   res.send('Hello from ticker details');
+})
+
+
+app.get('/api/companydescription/:ticker', (req, res) => {
+  const ticker = req.params.ticker.toUpperCase();
+
+  let requestOptions = {
+    'url': `https://api.tiingo.com/tiingo/daily/${ticker}?token=${TIINGO_API_KEY}`,
+    'method': 'GET',
+    'headers': {
+      'Content-Type': 'application/json',
+    }
+  };
+
+  request(requestOptions, (error, response, body) => {
+    let json = JSON.parse(body);
+    if (response.statusCode === 200) {
+      res.json(json);
+    } else {
+      res.json({'error': true});
+    }
+  });
 })
 
 app.listen(port, () => {
