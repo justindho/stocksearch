f9b7159042bffe6f349e72d312f327416f1de58f
justindho <justinho515@gmail.com>
2020-10-09 22:04:59 -0700

    Add styling to summary top banner

--- stocksearch/src/app/stock-banner/stock-banner.component.ts
+++ stocksearch/src/app/stock-banner/stock-banner.component.ts
@@ -33,6 +33,38 @@ export class StockBannerComponent implements OnInit {
         this.stockStatistics['change'] = (statistics.last - statistics.prevClose).toFixed(2);
         this.stockStatistics['changePercent'] = (this.stockStatistics['change'] / this.stockStatistics['prevClose'] * 100).toFixed(2);
         this.stockStatistics['timestamp'] = this.formatTimestamp(statistics.timestamp);
+
+        // Styling for when stock price goes up/down
+        let arrowContainer = document.getElementById('arrow-container');
+        let downArrow = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-caret-up-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
+                          </svg>`;
+        let upArrow = `<svg" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-caret-down-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                        </svg>`;
+        if (this.stockStatistics['change'] > 0) {
+          let green = "#319008";
+          arrowContainer.innerHTML = downArrow;
+          document.getElementById('lastPrice-cell').setAttribute('style', `color:${green}`);
+          document.getElementById('changeStats').setAttribute('style', `color:${green}`);
+        } else if (this.stockStatistics['change'] < 0) {
+          let red = "red";
+          arrowContainer.innerHTML = upArrow;
+          document.getElementById('lastPrice-cell').setAttribute('style', `color:${red}`);
+          document.getElementById('changeStats').setAttribute('style', `color:${red}`);
+        }
+
+        // Styling for when market is open/closed
+        let marketStatus = document.getElementById('market-status');
+        let now = new Date();
+        let lastTimestamp = new Date(this.stockStatistics.timestamp);
+        if ((now.getTime() - lastTimestamp.getTime()) / 1000 > 60*1000) { // convert seconds to milliseconds
+          marketStatus.innerHTML = `Market is Open`;
+          marketStatus.setAttribute('style', 'background-color:#DAF0E0; color:#78A48B');
+        } else {
+          marketStatus.innerHTML = `Market Closed on ${this.stockStatistics['timestamp']}`;
+          marketStatus.setAttribute('style', 'background-color:#F4D4DB; color:#85634E');
+        }
       });
   }
 
