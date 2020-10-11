f5c0ba3d4d571d4ed111ad803ffce7a5e5510414
justindho <justinho515@gmail.com>
2020-10-10 23:53:57 -0700

    Fix routing from seach page to stock details page

---
+++ stocksearch/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { StockSearchComponent } from './stock-search/stock-search.component';
+import { StockDetailComponent } from './stock-detail/stock-detail.component';
+
+
+const routes = [
+  { path: '', component: StockSearchComponent },
+  { path: 'search', redirectTo: '', pathMatch: 'full' },
+  { path: 'details/:symbol', component: StockDetailComponent },
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
