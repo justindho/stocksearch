76f04609f5da0d5f13a5c1ff502a5bb8c9ea538f
justindho <justinho515@gmail.com>
2020-10-10 21:42:27 -0700

    Add basic news modal structure

--- stocksearch/src/app/news/news.component.ts
+++ stocksearch/src/app/news/news.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 import { News } from '../news';
 import { StockService } from '../stock.service';
@@ -20,6 +21,15 @@ export class NewsComponent implements OnInit {
   getNews(): void {
     this.stockService.getNews()
       .subscribe(news => this.newslist = news);
+    this.newslist.forEach(news => news.publishedAt = this.formatTimestamp(news.publishedAt));
+  }
+
+  formatTimestamp(timestamp: string): string {
+    return moment(timestamp).format('MMMM D, YYYY');
+  }
+
+  constructTwitterUrl(text: string): string {
+    return `https://twitter.com/intent/tweet/text=${text}`
   }
 
 }
