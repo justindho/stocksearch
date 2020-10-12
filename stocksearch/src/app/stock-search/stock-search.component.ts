8049b3a049d35b55ff97699a3b49f7aa6b75ec6c
justindho <justinho515@gmail.com>
2020-10-12 14:40:26 -0700

    Add attempt at spinning loader for autocompletion

--- stocksearch/src/app/stock-search/stock-search.component.ts
+++ stocksearch/src/app/stock-search/stock-search.component.ts
@@ -1,12 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 import { Autocompletion } from '../autocompletion';
 import { StockService } from '../stock.service';
-import { Observable, Subject } from 'rxjs';
+import { of, iif, Observable, Subject } from 'rxjs';
 import {
   debounceTime, distinctUntilChanged, switchMap
 } from 'rxjs/operators';
+
+import { AutocompletionLoadingService } from '../autocompletion-loading.service';
+
+/** ATTEMPT TO GET SPINNING LOADER TO WORK AND DISAPPEAR WHEN LOADING IS DONE */
+// @Component({
+//   selector: 'app-stock-search',
+//   templateUrl: './stock-search.component.html',
+//   styleUrls: ['./stock-search.component.css']
+// })
+// export class StockSearchComponent implements OnInit {
+//   searchForm = new FormGroup({
+//     query: new FormControl('')
+//   })
+
+//   constructor(
+//     private autocompletionLoadingService: AutocompletionLoadingService,
+//     // private stockService: StockService
+//   ) {}
+
+//   ngOnInit(): void {}
+
+//   // Get loading stream from service
+//   loading$: Observable<boolean> = this.autocompletionLoadingService.loading$;
+
+//   // Deconstruct form to just take the query
+//   // Search on changes
+//   searchResults$ = this.searchForm.valueChanges.pipe(
+//     switchMap(({query}) => this.autocompletionLoadingService.get(query))
+//   );
+// }
+/** ATTEMPT TO GET SPINNING LOADER TO WORK AND DISAPPEAR WHEN LOADING IS DONE */
 
 @Component({
   selector: 'app-stock-search',
@@ -26,8 +57,8 @@ export class StockSearchComponent implements OnInit {
 
   ngOnInit(): void {
     this.autocompletions$ = this.searchTerms.pipe(
-      // wait 100ms after each keystroke before considering the next str
-      debounceTime(100),
+      // wait 300ms after each keystroke before considering the next str
+      debounceTime(300),
       // ignore new str if same as previous term
       distinctUntilChanged(),
       // switch to new search observable each time the str changes
