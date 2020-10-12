74c6af2c9731f61a15267470f4653ee94f66e499
justindho <justinho515@gmail.com>
2020-10-11 20:06:04 -0700

    Convert null values to -

--- stocksearch/src/app/summary-statistics/summary-statistics.component.ts
+++ stocksearch/src/app/summary-statistics/summary-statistics.component.ts
@@ -19,7 +19,25 @@ export class SummaryStatisticsComponent implements OnInit {
 
   getSummaryStatistics(): void {
     this.stockService.getStockStatistics('AMZN')
-      .subscribe(statistics => this.stockStatistics = statistics[0]);
+      .subscribe(statistics => {
+        this.stockStatistics = statistics[0];
+
+        // Style decimals
+        this.stockStatistics.high.toFixed(2);
+        this.stockStatistics.low.toFixed(2);
+        this.stockStatistics.open.toFixed(2);
+        this.stockStatistics.prevClose.toFixed(2);
+        if (this.stockStatistics.mid === null) parseFloat(this.stockStatistics.mid).toFixed(2);
+        if (this.stockStatistics.bidPrice === null) parseFloat(this.stockStatistics.bidPrice).toFixed(2);
+        if (this.stockStatistics.askPrice === null) parseFloat(this.stockStatistics.askPrice).toFixed(2);
+
+        // Check for null values
+        if (this.stockStatistics.mid === null) this.stockStatistics.mid = '-';
+        if (this.stockStatistics.bidPrice === null) this.stockStatistics.bidPrice = '-';
+        if (this.stockStatistics.bidSize === null) this.stockStatistics.bidSize = '-';
+        if (this.stockStatistics.askPrice === null) this.stockStatistics.askPrice = '-';
+        if (this.stockStatistics.askSize === null) this.stockStatistics.askSize = '-';
+      });
   }
 
 }
