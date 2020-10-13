47a711ac781eecf346c461ce4fb82e55fc460c18
justindho <justinho515@gmail.com>
2020-10-12 23:14:27 -0700

    Add stock/shares to local storage upon buying

--- stocksearch/src/app/buy-modal/buy-modal.component.ts
+++ stocksearch/src/app/buy-modal/buy-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { CompanyMeta } from '../company-meta';
 import { StockStatistics } from '../stock-statistics';
@@ -14,6 +14,50 @@ export class BuyModalComponent {
   total: string = "0.00";
 
   constructor(private modalService: NgbModal) { }
+
+  buy(): void {
+    let numShares = parseInt((<HTMLInputElement>document.getElementById('quantity')).value);
+    console.log(`numShares = ${numShares}`);
+    this.createPortfolio();
+    let portfolio = JSON.parse(localStorage.getItem('portfolio'));
+
+    if (this.companyMeta.ticker in portfolio) {
+      this.addSharesToPortfolio(numShares, portfolio);
+    } else {
+      this.addStockToPortfolio(numShares, portfolio);
+    }
+  }
+
+  addSharesToPortfolio(numShares: number, portfolio): void {
+    portfolio[this.companyMeta.ticker].quantity += numShares;
+    portfolio[this.companyMeta.ticker].totalCost += numShares * this.stockStatistics.last;
+    portfolio[this.companyMeta.ticker].avgCost = portfolio[this.companyMeta.ticker].totalCost / portfolio[this.companyMeta.ticker].quantity;
+    portfolio[this.companyMeta.ticker].change = portfolio[this.companyMeta.ticker].avgCost - this.stockStatistics.last;
+    portfolio[this.companyMeta.ticker].marketValue = this.stockStatistics.last * portfolio[this.companyMeta.ticker].quantity;
+    localStorage.setItem('portfolio', JSON.stringify(portfolio));
+  }
+
+  addStockToPortfolio(numShares: number, portfolio): void {
+    // The stock is not already in our portfolio
+    portfolio[this.companyMeta.ticker] = {
+      'ticker': this.companyMeta.ticker,
+      'name': this.companyMeta.name,
+      'quantity': numShares,
+      'avgCost': this.stockStatistics.last,
+      'totalCost': numShares * this.stockStatistics.last,
+      'change': 0, // avgCost - current price
+      'currentPrice': this.stockStatistics.last,
+      'marketValue': this.stockStatistics.last * numShares,
+    };
+    console.log(JSON.stringify(portfolio));
+    localStorage.setItem('portfolio', JSON.stringify(portfolio));
+  }
+
+  createPortfolio(): void {
+    if (localStorage.getItem('portfolio') === null) {
+      localStorage.setItem('portfolio', JSON.stringify({}));
+    }
+  }
 
   open(content) {
     this.modalService.open(content, {ariaLabelledBy:'buy-modal-title'});
