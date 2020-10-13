a954297dcee9c6c623afa3eeb0c395138fb8b7fe
justindho <justinho515@gmail.com>
2020-10-13 11:30:21 -0700

    Fix race conditions between getting data and displaying it

---
+++ stocksearch/src/app/buy-banner/buy-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buy-banner',
+  templateUrl: './buy-banner.component.html',
+  styleUrls: ['./buy-banner.component.css']
+})
+export class BuyBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
