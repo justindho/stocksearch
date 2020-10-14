8159a7b98979ce44e849c6fe044d1f40cfd9ff8f
justindho <justinho515@gmail.com>
2020-10-14 00:15:56 -0700

    Fix arrow indicators styling

--- stocksearch/src/app/stock-banner/stock-banner.component.ts
+++ stocksearch/src/app/stock-banner/stock-banner.component.ts
@@ -33,12 +33,12 @@ export class StockBannerComponent implements OnInit {
     this.stockStatistics['timestamp'] = this.formatTimestamp(this.stockStatistics.timestamp);
 
     // Styling for when stock price goes up/down
-    let arrowContainer = document.getElementById('arrow-container');
-    let downArrow = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-caret-up-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
+    let arrowContainer = document.getElementById('arrowContainer');
+    let downArrow = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-caret-down-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
                       </svg>`;
-    let upArrow = `<svg" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-caret-down-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+    let upArrow = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-caret-up-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
                     </svg>`;
     if (this.stockStatistics['change'] > 0) {
       let green = "#319008";
