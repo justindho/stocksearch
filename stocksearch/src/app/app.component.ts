d74a465cf0e064b69b12a1e7dcdaa70db255a030
justindho <justinho515@gmail.com>
2020-10-17 10:33:59 -0700

    Only highlight nav-item corresponding to the proper route

--- stocksearch/src/app/app.component.ts
+++ stocksearch/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  searchRoute: HTMLElement;
+  watchlistRoute: HTMLElement;
+  portfolioRoute: HTMLElement;
+  route: string;
   title = 'Stocksearch';
+
+  constructor(private router: Router) { }
+
+  ngAfterViewInit(): void {
+    this.searchRoute = document.getElementById('search-link');
+    this.watchlistRoute = document.getElementById('watchlist-link');
+    this.portfolioRoute = document.getElementById('portfolio-link');
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.route = event.url;
+        if (this.route === '/') {
+          this.searchRoute.classList.add('active');
+          this.watchlistRoute.classList.remove('active');
+          this.portfolioRoute.classList.remove('active');
+        } else if (this.route === '/watchlist') {
+          this.searchRoute.classList.remove('active');
+          this.watchlistRoute.classList.add('active');
+          this.portfolioRoute.classList.remove('active');
+        } else if (this.route === '/portfolio') {
+          this.searchRoute.classList.remove('active');
+          this.watchlistRoute.classList.remove('active');
+          this.portfolioRoute.classList.add('active');
+        } else if (this.route.startsWith('/details/')) {
+          this.searchRoute.classList.remove('active');
+          this.watchlistRoute.classList.remove('active');
+          this.portfolioRoute.classList.remove('active');
+        }
+      }
+    });
+  }
 }
